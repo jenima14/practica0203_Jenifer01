3265909fe9dfb6380c204eb1771053fee39a0601
jenima14 <jmarticarr2@educacion.navarra.es>
2023-11-03 20:30:11 +0100

    ejercicio_10

---
+++ ejercicio_10.py
@@ -0,0 +1,6 @@
+lista_precios = [50, 75, 46, 22, 80, 65, 8]
+menor_precio = min(lista_precios)
+mayor_precio = max(lista_precios)
+print("El menor precio es: ", menor_precio)
+print("El mayor precio es: ", mayor_precio)
+input()
